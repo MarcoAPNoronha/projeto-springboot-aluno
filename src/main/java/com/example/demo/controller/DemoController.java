396f205038650277e924225e273fc650f17af2e5
Marco Noronha <marcoapnoronha@hotmail.com>
2025-08-21 18:46:34 -0300

    Initial commit

---
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,28 @@
+package com.example.demo.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("demo")
+
+
+public class DemoController {
+
+
+    @GetMapping("/hello")
+    public String HelloDemo(){
+        return "Hi, im here.";
+    }
+
+
+    @GetMapping("/bye")
+    public String Bye(){
+        return "Bye, see you tomorrow.";
+    }
+
+
+
+}
